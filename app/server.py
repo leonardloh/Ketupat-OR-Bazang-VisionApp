33fe96ac5d688bbe9986b18cd979b7ec580c4f66
lohleonard93 <lohleonard93@gmail.com>
2019-08-03 13:39:41 +0800

    Update server.py
    update model url and object class

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1mbCWFz7x4YGzZwP30pUQewDyiq0psz-C?alt=media&key=AIzaSyDyBdV33lbtQiz6_JwKn9YYcnRJrSAu-lo'
+export_file_name = 'BazangORKetupat.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['bazang', 'ketupat']
 path = Path(__file__).parent
 
 app = Starlette()
